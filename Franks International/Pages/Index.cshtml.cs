16aa3ae71c90e8c65891a7f9ceaa44276e0b4eee
ShelbyFullbright <shelby.fullbright@gmail.com>
2020-03-15 16:20:54 -0600

    initial commit

---
+++ Franks International/Pages/Index.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ML;
+using Franks_InternationalML.Model;
+
+namespace Franks_International.Pages
+{
+    public class IndexModel : PageModel
+    {
+        //private readonly ILogger<IndexModel> _logger;
+        
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        private readonly PredictionEnginePool<ModelInput, ModelOutput> _predictionEnginePool;
+
+        public IndexModel(PredictionEnginePool<ModelInput, ModelOutput> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        [BindProperty]
+        public float userYear { get; set; }
+        [BindProperty]
+        public DateTime userMonth { get; set; }
+        [BindProperty]
+        public int UserDuration { get; set; }
+
+        List<float> lPredictionScore = new List<float>(); // initialization of list for predictions
+        List<int> lPredictionMonth = new List<int>(); //initialization of list of months
+
+        public void OnPost(int userYear, int userMonth, int userDuration, float userDayRevenue = 0)
+        {
+            for (int i = 0; i < userDuration; i++)
+            {
+                float userDay = DateTime.DaysInMonth(userYear, userMonth);
+                var input = new ModelInput { Year = userYear, Month = userMonth, Day = userDay, Day_Revenue = userDayRevenue };
+                var prediction = _predictionEnginePool.Predict(input);
+                ViewData["confirmation"] += $"{userMonth}-{userDay}-{userYear}, forecast is: {prediction.Score:C}<br />"; //testing output
+                lPredictionMonth.Add(userMonth); //add predicted month to list
+                lPredictionScore.Add(prediction.Score); //add prediction score to list
+                userMonth++;
+                if (userMonth > 12)
+                {
+                    userMonth = 1;
+                    userYear++;
+                }
+            }
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
